26f0cef3a9d850d36092a69e11d046cd6fdbc080
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-08-20 10:55:55 +0300

    creating TPC client

--- TCPserver.py
+++ TCPserver.py
@@ -1,14 +1,14 @@
-from http import server
+
 import socket
 
 #creating the socket object
-server.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #host = 192.168.0.103
-host = socket.gethostbyname
+host = socket.gethostname()
 port = 444
 
 #Binding to socket
-serversocket.bind('192.168.0.103', port)#host will be replaced|substituted with IP changed and not running on host
+serversocket.bind(('192.168.0.103', port))#host will be replaced|substituted with IP changed and not running on host
 
 #starting TCP listener
 serversocket.listen(3)
@@ -16,8 +16,8 @@ serversocket.listen(3)
 while True:
   #starting the connection
   clientsocket, address = serversocket.accept()
-  print ("received connection from %s" % str(address))
+  print ("received connection from : %s " % str(address))
 
-  massage = "hello! Thank you for contacting to the server" + "\r\n"
+  massage = "Hello! Thank you for contacting to the server" + "\r\n"
   clientsocket.send(massage.encode('ascii'))
   clientsocket.close()
