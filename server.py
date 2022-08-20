ac2c6828547c4b7bf9106baed951396527ed3242
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-08-20 09:20:48 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,23 @@
 from http import server
 import socket
 
+#creating the socket object
 server.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#host = 192.168.0.103
 host = socket.gethostbyname
 port = 444
-serversocket.bind(host, port)
+
+#Binding to socket
+serversocket.bind('192.168.0.103', port)#host will be replaced|substituted with IP changed and not running on host
+
+#starting TCP listener
 serversocket.listen(3)
+
 while True:
+  #starting the connection
   clientsocket, address = serversocket.accept()
-  print ("received connection from " % str(address))
+  print ("received connection from %s" % str(address))
 
   massage = "hello! Thank you for contacting to the server" + "\r\n"
-  clientsocket.send(massage)
+  clientsocket.send(massage.encode('ascii'))
   clientsocket.close()
